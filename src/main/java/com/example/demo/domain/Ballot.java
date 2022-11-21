76f114c326e5908028ce0aef36eb74a55c729d00
UmerAdilb <105809230+UmerAdilb@users.noreply.github.com>
2022-11-21 23:46:42 +0500

    delete vote sorted

--- src/main/java/com/example/demo/domain/Ballot.java
+++ src/main/java/com/example/demo/domain/Ballot.java
@@ -17,10 +17,10 @@ public class Ballot {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name="user_id",referencedColumnName = "id")
     private User user;
-    @ManyToOne(cascade = CascadeType.MERGE,fetch = FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "candidate_id")
     private Candidate candidate;
 }
---
+++ src/main/java/com/example/demo/exception/NoValueFound.java
@@ -0,0 +1,7 @@
+package com.example.demo.exception;
+
+public class NoValueFound extends RuntimeException{
+    public NoValueFound(String message){
+        super(message);
+    }
+}
